1003a2498ca76bfbb7f81555fe1788bbecf031a7
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-03-02 18:48:54 +0200

    1

---
+++ Unit Testing/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/05.Notification/app.js
@@ -0,0 +1,10 @@
+function notify(message) {
+  const notification = document.getElementById('notification');
+  notification.innerText = message;
+  notification.style.display = 'block';
+
+  notification.addEventListener('click', (event) => {
+    event.target.style.display = 'none';
+  });
+
+}
